c72ca8ce1bad51547582431068c8df2d565f1991
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-06 18:34:09 -0300

    feat: integrate couchbase for getproduct query

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import couchbase, { Bucket, Collection, GetResult } from "couchbase";
+import dotenv from 'dotenv';
 
 const typeDefs = `#graphql
     type Product {
@@ -16,13 +18,18 @@ const typeDefs = `#graphql
 
 const resolvers = {
     Query: {
-        async getProduct() {
-            return {
-                name: "Product 1",
-                price: 100,
-                quantity: 10,
-                tags: ["tag1", "tag2"],
-            };
+        async getProduct(_, args, contextValue) {
+            const { id } = args;
+
+            const bucket: Bucket = contextValue.couchbaseCluster.bucket('store-bucket');
+            const collection: Collection = bucket.scope('products-scope').collection('products');
+
+            const getResult: GetResult = await collection.get(id).catch((error) => {
+                console.error(error);
+                throw error;
+            });
+
+            return getResult.content;
         }
     }
 };
@@ -32,8 +39,20 @@ const server = new ApolloServer({
     resolvers,
 });
 
+dotenv.config();
+
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 }
+    listen: { port: 4000 },
+    context: async ({ req, res }) => ({
+        couchbaseCluster: await couchbase.connect(
+            process.env.COUCHBASE_CLUSTER,
+            {
+                username: process.env.COUCHBASE_USERNAME,
+                password: process.env.COUCHBASE_PASSWORD,
+                configProfile: "wanDevelopment",
+            }
+        )
+    })
 });
 
 console.log(`Server ready at ${url}`);
