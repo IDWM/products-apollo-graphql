76f05ad40a5760984b7fa51ff23e4a726d3498dc
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-06 16:50:37 -0300

    feat: add graphql schema and resolver for getproduct query

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,29 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `#graphql
+    type Product {
+        name: String
+        price: Float
+        quantity: Int
+        tags: [String]
+    }
+
+    type Query {
+        getProduct(id: String): Product
+    }
 `;
 
 const resolvers = {
+    Query: {
+        async getProduct() {
+            return {
+                name: "Product 1",
+                price: 100,
+                quantity: 10,
+                tags: ["tag1", "tag2"],
+            };
+        }
+    }
 };
 
 const server = new ApolloServer({
