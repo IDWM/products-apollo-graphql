1de341b91f90831fc14c53f63382716c7b4760d4
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-06 20:27:47 -0300

    feat: expand graphql schema and resolvers with new operations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import couchbase, { Bucket, BucketNotFlushableError, Cluster, Collection, GetResult, MutationResult, Scope } from "couchbase";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import couchbase, { Bucket, Collection, GetResult } from 'couchbase';
 import dotenv from 'dotenv';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -21,10 +21,14 @@ const typeDefs = `#graphql
 
     type Query {
         getProduct(id: String): Product
+        getProducts: [Product]
     }
 
     type Mutation {
         createProduct(product: ProductInput): Product
+        deleteProduct(id: String): Boolean
+        updateProduct(id: String, product: ProductInput): Product
+        setQuantity(id: String, quantity: Int): Boolean
     }
 `;
 
@@ -37,11 +41,34 @@ const resolvers = {
             const collection: Collection = bucket.scope('products-scope').collection('products');
 
             const getResult: GetResult = await collection.get(id).catch((error) => {
-                console.error(error);
+                console.log(error);
                 throw error;
             });
 
             return getResult.content;
+        },
+        async getProducts(_, __, contextValue) {
+            const result = await contextValue.couchbaseCluster.searchQuery(
+                'index-products',
+                couchbase.SearchQuery.matchAll()
+            )
+
+            const bucket: Bucket = contextValue.couchbaseCluster.bucket('store-bucket');
+            const collection: Collection = bucket.scope('products-scope').collection('products');
+
+            const products = [];
+
+            for (var i = 0; i < result.rows.length; i++) {
+                const id = result.rows[i].id;
+                const getResult: GetResult = await collection.get(id).catch((error) => {
+                    console.log(error);
+                    throw error;
+                });
+
+                products.push(getResult.content);
+            }
+
+            return products;
         }
     },
     Mutation: {
@@ -51,17 +78,60 @@ const resolvers = {
             const bucket: Bucket = contextValue.couchbaseCluster.bucket('store-bucket');
             const collection: Collection = bucket.scope('products-scope').collection('products');
 
-            const key: string = uuidv4();
+            const key = uuidv4();
 
-            const createdMutation: MutationResult = await collection.insert(key, product).catch((error) => {
-                console.error(error);
+            await collection.insert(key, product).catch((error) => {
+                console.log(error);
                 throw error;
             });
 
             return product;
+        },
+        async deleteProduct(_, args, contextValue) {
+            const { id } = args;
+
+            const bucket: Bucket = contextValue.couchbaseCluster.bucket('store-bucket');
+            const collection: Collection = bucket.scope('products-scope').collection('products');
+
+            await collection.remove(id).catch((error) => {
+                console.log(error);
+                throw error;
+            });
+
+            return true;
+        },
+        async updateProduct(_, args, contextValue) {
+            const { id, product } = args;
+
+            const bucket: Bucket = contextValue.couchbaseCluster.bucket('store-bucket');
+            const collection: Collection = bucket.scope('products-scope').collection('products');
+
+            await collection.replace(id, product).catch((error) => {
+                console.log(error);
+                throw error;
+            });
+
+            return product;
+        },
+        async setQuantity(_, args, contextValue) {
+            const { id, quantity } = args;
+
+            const bucket: Bucket = contextValue.couchbaseCluster.bucket('store-bucket');
+            const collection: Collection = bucket.scope('products-scope').collection('products');
+
+            await collection.mutateIn(id,
+                [
+                    couchbase.MutateInSpec.replace('quantity', quantity)
+                ]
+            ).catch((error) => {
+                console.log(error);
+                throw error;
+            });
+
+            return true;
         }
     }
-};
+}
 
 const server = new ApolloServer({
     typeDefs,
@@ -78,7 +148,7 @@ const { url } = await startStandaloneServer(server, {
             {
                 username: process.env.COUCHBASE_USERNAME,
                 password: process.env.COUCHBASE_PASSWORD,
-                configProfile: "wanDevelopment",
+                configProfile: 'wanDevelopment',
             }
         )
     })
